bd16a93a2535f48d654af2ed86a74e8d9683c35e
kilvistyle <satoshi.chiba@codeberry.co.jp>
2019-07-02 02:40:05 +0900

    Serializable漏れ

--- src/main/java/com/example/reactor/resource/Entry.java
+++ src/main/java/com/example/reactor/resource/Entry.java
@@ -6,6 +6,7 @@ import com.example.reactor.entity.Header;
 import lombok.Builder;
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.List;
 
 /**
@@ -20,7 +21,7 @@ import java.util.List;
  */
 @Data
 @Builder
-public class Entry {
+public class Entry implements Serializable {
 
     public Header header;
     public Body body;
