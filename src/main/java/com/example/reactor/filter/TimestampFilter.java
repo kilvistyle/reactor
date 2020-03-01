49e9c1d882b256448c1fb1fd6700d0077cc1dc14
kilvistyle <kilvistyle@gmail.com>
2020-03-02 00:09:04 +0900

    WebFilterのサンプル作成

--- src/main/java/com/example/reactor/filter/TimestampFilter.java
+++ src/main/java/com/example/reactor/filter/TimestampFilter.java
@@ -2,16 +2,14 @@ package com.example.reactor.filter;
 
 import lombok.extern.slf4j.Slf4j;
 import org.reactivestreams.Publisher;
-import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.WebFilter;
-import org.springframework.web.server.WebFilterChain;
+import org.springframework.web.server.*;
 import reactor.core.publisher.Mono;
+
+import java.io.IOException;
 
 @Slf4j
 @Component
-@Order(1)
 public class TimestampFilter implements WebFilter {
 
     @Override
@@ -34,18 +32,21 @@ public class TimestampFilter implements WebFilter {
     private void doBeforeRequest(ServerWebExchange exchange) {
         String uri = exchange.getRequest().getURI().toString();
         long start = System.currentTimeMillis();
+        // start log（ex: /hoge [IN]: 1582989973940）
         log.info(String.format("%s [IN]: %d", uri, start));
     }
 
     private void doAfterRequest(ServerWebExchange exchange) {
         String uri = exchange.getRequest().getURI().toString();
         long end = System.currentTimeMillis();
+        // end log（ex: /hoge [OUT]: 1582989974053）
         log.info(String.format("%s [OUT]: %d", uri, end));
     }
 
     private void doAfterRequestWithError(ServerWebExchange exchange, Throwable throwable) {
         String uri = exchange.getRequest().getURI().toString();
         long end = System.currentTimeMillis();
+        // end with error log（ex: /hoge [OUT]: 1582989974053 with Error(java.lang.RuntimeException)）
         log.info(String.format("%s [OUT]: %d with Error(%s)", uri, end, throwable.toString()));
     }
 }
